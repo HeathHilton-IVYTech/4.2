5e27f478097f75d948fb22b4e2f89b89c66577fa
Heath Hilton <hhilton2@ivytech.edu>
2022-11-06 17:04:19 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# Heath Hilton
+# 11/06/2022
+
+# Program: 4.2 - Small & Green
+
+# The program is using very basic characteristics to figure out which item the user has.
+
+# Import the random module
+small = True
+green = True
+
+print("You say that your item is small and green, let's see if we can figure out what it is.")
+
+if small == True:
+    if green == True:
+        print("Your small and green item is a pea.")
+    else:
+        print("Your small and not green item is a cherry.")
+else:
+    if green == True:
+        print("Your large and green item is a watermelon.")
+    else:
+        print("Your large and not green item is a pumpkin.")
